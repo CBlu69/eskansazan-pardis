74a89e1ab387b8a1b460d6966ff3889eb65138c9
C Blu <107987692+CBlu69@users.noreply.github.com>
2026-06-16 22:15:39 +0330

    Update app.js

--- app.js
+++ app.js
@@ -88,9 +88,12 @@ function initMap() {
 
     map = L.map("map").setView([35.6892, 51.3890], 6);
 
-   L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-    attribution: "© OpenTopoMap"
-}).addTo(map);
+L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution: "Tiles © Esri"
+  }
+).addTo(map);
 }
 function renderMarkers() {
     if (!map) return;
